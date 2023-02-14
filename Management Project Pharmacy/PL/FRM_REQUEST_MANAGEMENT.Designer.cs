80abcc45fbd42c56e5fdb78ddc60084d2e3ddeb0
assem-qashah <119502077+assem-qashah@users.noreply.github.com>
2023-02-14 21:10:50 +0300

    Add new from

---
+++ Management Project Pharmacy/PL/FRM_REQUEST_MANAGEMENT.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Pharmacy_Managment.PL
+{
+    partial class FRM_REQUEST_MANAGEMENT
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FRM_REQUEST_MANAGEMENT));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.grid_request = new System.Windows.Forms.DataGridView();
+            this.btn_display = new System.Windows.Forms.Button();
+            this.btn_print = new System.Windows.Forms.Button();
+            this.btn_cancel = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grid_request)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.grid_request);
+            this.groupBox1.Location = new System.Drawing.Point(12, 42);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(799, 306);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "كل فواتير الشراء";
+            // 
+            // grid_request
+            // 
+            this.grid_request.AllowUserToAddRows = false;
+            this.grid_request.AllowUserToDeleteRows = false;
+            this.grid_request.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.grid_request.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.grid_request.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grid_request.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grid_request.Location = new System.Drawing.Point(3, 16);
+            this.grid_request.Name = "grid_request";
+            this.grid_request.ReadOnly = true;
+            this.grid_request.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.grid_request.Size = new System.Drawing.Size(793, 287);
+            this.grid_request.TabIndex = 0;
+            // 
+            // btn_display
+            // 
+            this.btn_display.Image = ((System.Drawing.Image)(resources.GetObject("btn_display.Image")));
+            this.btn_display.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_display.Location = new System.Drawing.Point(668, 354);
+            this.btn_display.Name = "btn_display";
+            this.btn_display.Size = new System.Drawing.Size(140, 39);
+            this.btn_display.TabIndex = 1;
+            this.btn_display.Text = "عرض كل الفواتير";
+            this.btn_display.UseVisualStyleBackColor = true;
+            this.btn_display.Click += new System.EventHandler(this.btn_display_Click);
+            // 
+            // btn_print
+            // 
+            this.btn_print.Image = ((System.Drawing.Image)(resources.GetObject("btn_print.Image")));
+            this.btn_print.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_print.Location = new System.Drawing.Point(462, 354);
+            this.btn_print.Name = "btn_print";
+            this.btn_print.Size = new System.Drawing.Size(147, 39);
+            this.btn_print.TabIndex = 2;
+            this.btn_print.Text = "طباعة الفاتورة المحدده";
+            this.btn_print.UseVisualStyleBackColor = true;
+            this.btn_print.Click += new System.EventHandler(this.btn_print_Click);
+            // 
+            // btn_cancel
+            // 
+            this.btn_cancel.BackColor = System.Drawing.Color.Crimson;
+            this.btn_cancel.Image = ((System.Drawing.Image)(resources.GetObject("btn_cancel.Image")));
+            this.btn_cancel.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_cancel.Location = new System.Drawing.Point(26, 354);
+            this.btn_cancel.Name = "btn_cancel";
+            this.btn_cancel.Size = new System.Drawing.Size(89, 39);
+            this.btn_cancel.TabIndex = 3;
+            this.btn_cancel.Text = "خروج";
+            this.btn_cancel.UseVisualStyleBackColor = false;
+            this.btn_cancel.Click += new System.EventHandler(this.btn_cancel_Click);
+            // 
+            // FRM_REQUEST_MANAGEMENT
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightBlue;
+            this.ClientSize = new System.Drawing.Size(823, 405);
+            this.Controls.Add(this.btn_cancel);
+            this.Controls.Add(this.btn_print);
+            this.Controls.Add(this.btn_display);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "FRM_REQUEST_MANAGEMENT";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.Text = "ادارة المشتريات";
+            this.Load += new System.EventHandler(this.FRM_REQUEST_MANAGEMENT_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.grid_request)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView grid_request;
+        private System.Windows.Forms.Button btn_display;
+        private System.Windows.Forms.Button btn_print;
+        private System.Windows.Forms.Button btn_cancel;
+    }
+}
---
+++ Management Project Pharmacy/PL/FRM_SCEINTIFICMANAGEMENT.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Pharmacy_Managment.BL;
+
+namespace Pharmacy_Managment.PL
+{
+    public partial class FRM_SCEINTIFICMANAGEMENT : Form
+    {
+        public FRM_SCEINTIFICMANAGEMENT()
+        {
+            InitializeComponent();
+            btnSelectALL_Click(null, null);
+        }
+
+        private void btnSelectALL_Click(object sender, EventArgs e)
+        {
+            DataTable DT = CLASS_SCEINTIFICNAME.SP_SELCTALLSCEINTIFICNAME();
+            dgvSceintifiecName.DataSource = DT;
+        }
+
+        private void dgvSceintifiecName_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if(e.RowIndex >= 0)
+            {
+                txtSNID.Text = dgvSceintifiecName.Rows[e.RowIndex].Cells[0].Value.ToString();
+                 txtSnName.Text = dgvSceintifiecName.Rows[e.RowIndex].Cells[1].Value.ToString();
+
+            }
+        }
+
+        private void btnDisplay_Click(object sender, EventArgs e)
+        {
+            DataTable DT = CLASS_SCEINTIFICNAME.SP_SELCTSERCHSCEINTIFICNAME(txtSearch.Text);
+            dgvSceintifiecName.DataSource = DT;
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            CLASS_SCEINTIFICNAME.SP_SCEINTIFICNAMEUPATE(int.Parse(txtSNID.Text), txtSnName.Text);
+            MessageBox.Show("تم التعديل بنجاح");
+            btnSelectALL_Click(null, null);
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            CLASS_SCEINTIFICNAME.SP_SCEINTIFICNAMEDELETE(int.Parse(txtSNID.Text));
+            MessageBox.Show("تم الحذف");
+            btnSelectALL_Click(null, null);
+        }
+
+        private void btnPrint_Click(object sender, EventArgs e)
+        {
+            RPT.FRM_REPORTS frm = new RPT.FRM_REPORTS();
+            RPT.RPT_ALLACTIVEMATERIAL actm = new RPT.RPT_ALLACTIVEMATERIAL();
+            frm.crystalReportViewer1.ReportSource = actm;
+            frm.ShowDialog();
+        }
+    }
+}
